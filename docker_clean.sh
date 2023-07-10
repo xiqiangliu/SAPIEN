e227006c3f6647aa10dd32fe27bc6f3b0c041825
Xiqiang Liu <9440183+xiqiangliu@users.noreply.github.com>
2023-07-10 12:57:03 -0700

    fix docker tag

--- docker_clean.sh
+++ docker_clean.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 docker run -v `pwd`:/workspace/SAPIEN -it --rm \
-       fxiangucsd/sapien-build-env bash -c "cd /workspace/SAPIEN && rm -rf build wheelhouse dist sapien.egg-info"
+       fxiangucsd/sapien-build-env:3.0 bash -c "cd /workspace/SAPIEN && rm -rf build wheelhouse dist sapien.egg-info"
